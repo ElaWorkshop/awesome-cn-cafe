f3db960288535e937df493838662ad138d90b0b2
Xavier Zhou <xiayezhou@gmail.com>
2016-02-18 22:19:50 +0800

    Iterate all cities instead of specify one manually

--- tool/README.md
+++ tool/README.md
@@ -1,21 +1,16 @@
 ## Purpose
 This script automatically sets the `marker-color` for shops by the different network speed.
 
-```js
-const RED = '#C24740'; // 0~5M
-const YELLOW = '#F3AE1A'; // 5~10M
-const GREEN = '#50C240'; // 10~M
-```
+Color | HEX | Speed
+------ | ------ | -----
+RED | #C24740 | 0~5M
+YELLOW | #F3AE1A | 5~10M
+GREEN | #50C240 | 10~M
 
 ## Prequisite
 [Node.js](https://nodejs.org/)
 
 ## How to run
 
-`node mark_color.js <city>`
-
-e.g.
-
-`node mark_color.js shanghai`
-
+`node tool/build_marker.js`
 
--- tool/mark_color.js
+++ tool/mark_color.js
@@ -8,42 +8,47 @@ const RED = '#C24740';
 const YELLOW = '#F3AE1A';
 const GREEN = '#50C240';
 
-const city = process.argv[2];
-if (!city || !city.match(/(shanghai|beijing|nanjing)/)) {
-  console.log(`Missing City! Usage:
-    node addColor.js <city>[shanghai|beijing|nanjing]`);
-  process.exit(1);
+const cities = ['shanghai', 'beijing', 'nanjing'];
+
+const setMarkerSymbol = (feature) => {
+  feature.properties['marker-symbol'] || (feature.properties['marker-symbol'] = 'cafe');
 }
-const sourceFile = path.resolve(__dirname, `../${city}.geojson`);
-const tempFile = path.resolve(__dirname, './tmp.json');
 
-//copy geojson to json file because node.js only requires(load) json file.
-fs.writeFileSync(tempFile, fs.readFileSync(sourceFile));
-
-let target = require(tempFile);
-
-for (let f of target.features) {
-  let downloadSpeed = f.properties['下载速度'];
-  //some downloadSpeed may be array, in such case let's caculate the average
-  if (!Array.isArray(downloadSpeed)) {
-    downloadSpeed = [ downloadSpeed ];
-  }
-  let sum = downloadSpeed.map(value => {
-    const matched = value.match(/^([\d|\.]+)\s?Mbps$/i);
-    let speedStr = matched && matched[1];
-    assert(speedStr);
-    return parseFloat(speedStr);
-  }).reduce((a, b) => { return a + b });
-  let avg = sum / downloadSpeed.length;
-  f.properties['marker-symbol'] = 'cafe';
+const setMarkerColor = (feature, avg) => {
   if (avg < 5) {
-    f.properties['marker-color'] = RED;
+    feature.properties['marker-color'] = RED;
   } else if (avg < 10) {
-    f.properties['marker-color'] = YELLOW;
+    feature.properties['marker-color'] = YELLOW;
   } else {
-    f.properties['marker-color'] = GREEN;
+    feature.properties['marker-color'] = GREEN;
   }
 }
-fs.unlinkSync(tempFile);
-fs.writeFileSync(sourceFile, JSON.stringify(target, null, 2) + '\n');
-console.log(`Done with ${target.features.length} records!`);
+
+const buildMarker = (city) => {
+  const sourceFile = path.resolve(__dirname, `../${city}.geojson`);
+  const tempFile = path.resolve(__dirname, `./tmp.${city}.json`);
+
+  //copy geojson to json file because node.js only requires(load) json file.
+  fs.writeFileSync(tempFile, fs.readFileSync(sourceFile));
+  let target = require(tempFile);
+  for (let f of target.features) {
+    let downloadSpeed = f.properties['下载速度'];
+    //some downloadSpeed may be array, in such case let's caculate the average
+    if (!Array.isArray(downloadSpeed)) {
+      downloadSpeed = [ downloadSpeed ];
+    }
+    let sum = downloadSpeed.map(value => {
+      const matched = value.match(/^([\d|\.]+)\s?Mbps$/i);
+      let speedStr = matched && matched[1];
+      assert(speedStr);
+      return parseFloat(speedStr);
+    }).reduce((a, b) => { return a + b });
+    let average = sum / downloadSpeed.length;
+    setMarkerColor(f, average);
+    setMarkerSymbol(f);
+  }
+  fs.unlinkSync(tempFile);
+  fs.writeFileSync(sourceFile, JSON.stringify(target, null, 2) + '\n');
+  console.log(`${city}: Done with ${target.features.length} records!`);
+}
+cities.forEach(buildMarker)
